5a6b3d0a7a672b5278f0cda68d63d98e3fe6e3ee
Jacob Muchow <jacobamuchow@gmail.com>
2016-06-02 18:03:35 -0500

    Move collapse animation to another method to expose it for gestures later; added method comments.

--- dynamicviewpager/src/main/java/com/quarkworks/dynamicviewpager/DynamicPagerAdapter.java
+++ dynamicviewpager/src/main/java/com/quarkworks/dynamicviewpager/DynamicPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.quarkworks.dynamicviewpager;
 
+import android.support.annotation.Nullable;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,7 +43,16 @@ abstract public class DynamicPagerAdapter extends PagerAdapter {
         return view == object;
     }
 
-    public void discardView(final View view, final OnDiscardFinishedCallback onDiscardFinishedCallback) {
+    /**
+     * This will fade out the View passed in, then call collapseViewsIn() to finish the animation
+     * (resembles a deletion).
+     *
+     * @param view The view to hide and collapse around. You will usually want to use the current
+     * item of the ViewPager.
+     * @param onDiscardFinishedCallback This gets called when the animations have finished. If you
+     * are deleting an item, you should remove it from your data set and call notifyDataSetChanged().
+     */
+    public void discardView(final View view, @Nullable final OnDiscardFinishedCallback onDiscardFinishedCallback) {
 
         AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
         alphaAnimation.setDuration(300);
@@ -51,71 +61,93 @@ abstract public class DynamicPagerAdapter extends PagerAdapter {
         alphaAnimation.setAnimationListener(new SimpleAnimationListener() {
             @Override
             public void onAnimationEnd(Animation animation) {
-
-                /**
-                 * Get position for current View
-                 */
-                int position = -1;
-
-                for(Map.Entry<Integer, View> entry : children.entrySet()) {
-                    if(entry.getValue().equals(view)) {
-                        position = entry.getKey();
-                    }
-                }
-
-                if(position == -1) {
-                    onDiscardFinishedCallback.onDiscardFinished();
-                    return;
-                }
-
-                /**
-                 * Get next view (check right first, then left)
-                 */
-                View nextView = children.get(position+1);
-                View farNextView = children.get(position+2);
-
-                if(nextView == null) {
-                    nextView = children.get(position-1);
-                    farNextView = children.get(position-2);
-                }
-
-                /**
-                 * If it is still null, just discard the current
-                 */
-                if(nextView == null) {
-                    onDiscardFinishedCallback.onDiscardFinished();
-                    return;
-                }
-
-                /**
-                 * Animate the next card in
-                 */
-                float toXDelta = view.getX() - nextView.getX();
-
-                TranslateAnimation nextViewAnimation = new TranslateAnimation(0, toXDelta, 0, 0);
-                nextViewAnimation.setDuration(400);
-                nextViewAnimation.setFillAfter(true);
-
-                nextViewAnimation.setAnimationListener(new SimpleAnimationListener() {
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        onDiscardFinishedCallback.onDiscardFinished();
-                    }
-                });
-
-                TranslateAnimation farNextViewAnimation = new TranslateAnimation(0, toXDelta, 0, 0);
-                farNextViewAnimation.setDuration(400);
-                farNextViewAnimation.setFillAfter(true);
-
-                nextView.startAnimation(nextViewAnimation);
-
-                if(farNextView != null) {
-                    farNextView.startAnimation(farNextViewAnimation);
-                }
+                collapseViewsIn(view, onDiscardFinishedCallback);
             }
         });
 
         view.startAnimation(alphaAnimation);
+    }
+
+    /**
+     * This will look for views to the right, then the left, to animate to the position of the View
+     * passed in. This is used mainly for discardView(), but it is being exposed for gesture dismissals.
+     *
+     * @param view The view to collapse around. You will usually want to use the current item
+     * of the ViewPager.
+     * @param onDiscardFinishedCallback This gets called when the animations have finished. If you
+     * are deleting an item, you should remove it from your data set and call notifyDataSetChanged().
+     */
+    public void collapseViewsIn(View view, @Nullable final OnDiscardFinishedCallback onDiscardFinishedCallback) {
+
+        /**
+         * Get position for current View
+         */
+        int position = -1;
+
+        for(Map.Entry<Integer, View> entry : children.entrySet()) {
+            if(entry.getValue().equals(view)) {
+                position = entry.getKey();
+            }
+        }
+
+        /**
+         * Stop if a position can't be found
+         */
+        if(position == -1) {
+            if(onDiscardFinishedCallback != null) {
+                onDiscardFinishedCallback.onDiscardFinished();
+            }
+            return;
+        }
+
+        /**
+         * Get next view (check right first, then left)
+         */
+        View nextView = children.get(position+1);
+        View farNextView = children.get(position+2);
+
+        if(nextView == null) {
+            nextView = children.get(position-1);
+            farNextView = children.get(position-2);
+        }
+
+        /**
+         * If it is still null, just discard the current
+         */
+        if(nextView == null) {
+            if(onDiscardFinishedCallback != null) {
+                onDiscardFinishedCallback.onDiscardFinished();
+            }
+            return;
+        }
+
+        /**
+         * Start collapsing animations
+         */
+        float toXDelta = view.getX() - nextView.getX();
+
+        TranslateAnimation nextViewAnimation = new TranslateAnimation(0, toXDelta, 0, 0);
+        nextViewAnimation.setDuration(400);
+        nextViewAnimation.setFillAfter(true);
+
+        nextViewAnimation.setAnimationListener(new SimpleAnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if(onDiscardFinishedCallback != null) {
+                    onDiscardFinishedCallback.onDiscardFinished();
+                }
+            }
+        });
+
+        TranslateAnimation farNextViewAnimation = new TranslateAnimation(0, toXDelta, 0, 0);
+        farNextViewAnimation.setDuration(400);
+        farNextViewAnimation.setFillAfter(true);
+
+        nextView.startAnimation(nextViewAnimation);
+
+        if(farNextView != null) {
+            farNextView.startAnimation(farNextViewAnimation);
+        }
     }
 
     public abstract View instantiateView(ViewGroup container, int position);
